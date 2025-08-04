d8ba454e2b746e6e75b4f51711143d957757f8b1
Developer Mspl <developer.mspl@gmail.com>
2025-08-04 18:01:37 +0530

    ....

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,4 +1,5 @@
 
 
 
-export const BASE_URL: string = 'https://marier.one:9001/api/v1';
+export const BASE_URL: string = 'https://servicesapi.meander.software/api#';
+// export const BASE_URL: string = 'https://marier.one:9001/api/v1';
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,18 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+// metro.config.js
+const { getDefaultConfig } = require('metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
